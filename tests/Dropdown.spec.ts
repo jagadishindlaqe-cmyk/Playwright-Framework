17f6f3838f11d1e09d8f60adc23e853d3e7386b4
Jagadish Indla <jagadishindla.qe@gmail.com>
2026-07-28 08:47:46 +0530

    Fix dropdown test syntax

--- tests/Dropdown.spec.ts
+++ tests/Dropdown.spec.ts
@@ -3,8 +3,6 @@ test('verify the page',async({page})=>{
     await page.goto("https://the-internet.herokuapp.com/dropdown");
     //await page.selectOption('1')
    const option2= await page.selectOption('dropdown','Option 2');
-   await expect(option2).toBeVisible();
+   await expect(option2).toEqual(['2']);
 
-}
-
-})
+});
