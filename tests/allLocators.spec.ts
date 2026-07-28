8d0e90f4b74ed4d14439b082b83b40ccf0d53375
Jagadish Indla <jagadishindla.qe@gmail.com>
2026-07-28 09:41:59 +0530

    smoke

--- tests/allLocators.spec.ts
+++ tests/allLocators.spec.ts
@@ -1,6 +1,6 @@
 import{test,expect} from '@playwright/test';
 
-test("verify all the locators",async({page})=>{
+test("verify all the locators @smoke",async({page})=>{
 await page.goto("https://www.saucedemo.com/")
 await page.locator("#user-name").fill("standard_user")
 await page.locator("#password").fill("secret_sauce")
